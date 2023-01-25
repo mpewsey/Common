c745e51b9afa89e9cce7f9fa6d0e2b2d7a32295d
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-01-24 23:01:23 -0500

    Add logger

---
+++ src/Common.Tests/Logging/TestLogger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MPewsey.Common.Logging.Tests
+{
+    [TestClass]
+    public class TestLogger
+    {
+        [TestMethod]
+        public void TestLog()
+        {
+            Logger.RemoveAllListeners();
+            var messages = new List<string>();
+            var expected = new List<string> { "Message1", "Message2", "Message2" };
+
+            // Add the listener.
+            Logger.AddListener(messages.Add);
+
+            foreach (var message in expected)
+            {
+                Logger.Log(message);
+            }
+
+            CollectionAssert.AreEqual(expected, messages);
+
+            // Remove the listener.
+            Logger.RemoveListener(messages.Add);
+
+            foreach (var message in expected)
+            {
+                Logger.Log(message);
+            }
+
+            CollectionAssert.AreEqual(expected, messages);
+        }
+    }
+}
