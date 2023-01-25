0635124c65639cc0648be08f3bac2345fe3ebad5
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-01-25 00:24:19 -0500

    Add pipeline

---
+++ src/Common/Pipelines/IPipelineStep.cs
@@ -0,0 +1,15 @@
+﻿namespace MPewsey.Common.Pipelines
+{
+    /// <summary>
+    /// An interface for creating a step of a Pipeline.
+    /// </summary>
+    public interface IPipelineStep
+    {
+        /// <summary>
+        /// Performs the operations for this step.
+        /// Artifacts should be written to the results outputs.
+        /// </summary>
+        /// <param name="results">The pipeline results.</param>
+        bool ApplyStep(PipelineResults results);
+    }
+}
---
+++ src/Common/Pipelines/Pipeline.cs
@@ -0,0 +1,48 @@
+﻿using MPewsey.Common.Logging;
+using System.Collections.Generic;
+
+namespace MPewsey.Common.Pipelines
+{
+    /// <summary>
+    /// A class for chaining multiple IPipelineStep together.
+    /// </summary>
+    public class Pipeline
+    {
+        /// <summary>
+        /// A list of pipeline steps.
+        /// </summary>
+        public List<IPipelineStep> Steps { get; private set; } = new List<IPipelineStep>();
+
+        /// <summary>
+        /// Initializes a new pipeline.
+        /// </summary>
+        /// <param name="steps">The pipeline steps.</param>
+        public Pipeline(params IPipelineStep[] steps)
+        {
+            Steps = new List<IPipelineStep>(steps);
+        }
+
+        /// <summary>
+        /// Invokes all generators of the pipeline and returns the results.
+        /// </summary>
+        /// <param name="inputs">A dictionary of generator inputs.</param>
+        public PipelineResults Generate(Dictionary<string, object> inputs)
+        {
+            Logger.Log("Running pipeline...");
+            var results = new PipelineResults(inputs);
+
+            foreach (var step in Steps)
+            {
+                if (!step.ApplyStep(results))
+                {
+                    Logger.Log("Pipeline failed.");
+                    return results;
+                }
+            }
+
+            results.Complete();
+            Logger.Log("Pipeline complete.");
+            return results;
+        }
+    }
+}
